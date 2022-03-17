8637c24a184e0d7c2739669f1d784350fa5525ec
shuto1998 <shuto.y0319@gmail.com>
2022-03-17 13:22:02 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 
  namespace :admin do
-   resources :genres, only: [:new,:create,:index,:edit]
+   resources :genres, only: [:new,:create,:index,:edit,:update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
