f4d431d4b13a6c86fad3c4bb9260435d3a86dda5
shuto1998 <shuto.y0319@gmail.com>
2022-03-28 13:09:41 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -27,7 +27,9 @@ class Admin::ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
+    
     item.update(item_params)
+    
     redirect_to admin_item_path(item.id)
   end
 
