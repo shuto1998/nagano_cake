023db82d4f2d300e93d312af86cbe08b53ef8317
EC2 Default User <ec2-user@ip-172-31-41-24.ap-northeast-1.compute.internal>
2022-04-07 11:13:53 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,20 +6,27 @@ class Public::OrdersController < ApplicationController
   end
 
   def new
-    @orders = Order.all
+    # @orders = Order.all
     @order = Order.new
 
-    # @order.postal_code = current_customer.postal_code
-    # @order.address = current_customer.address
-    # @order.name = current_customer.first_name + current_customer.last_name
+    # @customer.postal_code = current_customer.postal_code
+    # @customer.address = current_customer.address
+    # @customer.name = current_customer.first_name + current_customer.last_name
 
   end
 
-  # def create
-  #   order = Order.new(order_params)
-  #   order.save
-  #   redirect_to comfirm_orders_path
-  # end
+  def create
+    
+    order = Order.new(order_params)
+    order.save
+    OrderDetail.create!(
+     customer_id: 1,
+     order_id: 1,
+  )
+    cart_item.destroy_all
+    redirect_to "complete_orders_path"
+ 
+  end
 
   def comfirm
      #カートアイテムの定義
@@ -27,13 +34,19 @@ class Public::OrdersController < ApplicationController
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
 
     @order = Order.new(order_params)
+   
+
     if params[:order][:select_address] == "0"
+      #@orderの住所・郵便番号・名前は現在ログインしているcustomreの物  であると定義するコード
           @order.postal_code = current_customer.postal_code
           @order.address = current_customer.address
           @order.name = current_customer.first_name + current_customer.last_name
 
     elsif  params[:order][:select_address] == "1"
+      #登録されているaddressを探してくるコード
+      #orderパラメーターからaddress_idを探してくる
          @address = Address.find(params[:order][:address_id])
+      #探してきたものを@orderに定義するコード
          @order.postal_code = @address.postal_code
          @order.address = @address.address
          @order.name = @address.name
@@ -41,12 +54,24 @@ class Public::OrdersController < ApplicationController
 
     elsif  params[:order][:select_address] == "2"
 
-
+      #アドレスを新規で登録するコード
+      #params に情報を入れる（今回の場合address_paramsに:postal_code,:address,:nameの情報が入っているのでその3つが登録できる）
+      @address = Address.new(address_params)
+      #登録したaddressの情報が誰のものか登録する。addressを登録するid=現在のaddressのid
+      @address.customer_id = current_customer.id
+      #作成したアドレスを保存する
+      @address.save
+      #作成したアドレスを@orderの変数に定義するコード
+      #@orderのpostal_codeは作成した@addressのpostal_codeということを定義
+      @order.postal_code = @address.postal_code
+      #@orderのaddressは作成した@addressのaddressということを定義
+      @order.address = @address.address
+      #@orderのnameは作成した@addressのnameということを定義
+      @order.name = @address.name
     end
-    # @order = Order.new(order_params)
-    # @order.postal_code = current_customer.postal_code
-    # @order.address = current_customer.address
-    # @order.name = current_customer.first_name + current_customer.last_name
+    #shippong_costカラムは800である問いことを定義
+    @order.shipping_cost = 800
+    @order.status = 0
   end
 
   def complete
@@ -56,6 +81,10 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:customer_id,:postal_code,:address,:name,:shipping_cost,:total_payment,:status,:payment_method)
+  end
+
+  def address_params
+     params.require(:order).permit(:postal_code,:address,:name)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
      email: 'test@tesr.com',
      password: 'testpass',
   )
+  
+  
